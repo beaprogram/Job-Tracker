674fd8123392ff4959e1633f00030968abad98d0
Arup Halder <aruphalder4602@gmail.com>
2026-01-18 14:38:03 -0400

    Added API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,26 +1,25 @@
 import axios from 'axios';
 
-const API=axios.create({
-    baseURL: 'http://localhost:3000/api'
+const API = axios.create({
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api'
 });
 
-API.interceptors.request.use((config)=>{
-    const token=localStorage.getItem('token');
-    if(token)
-    {
-        config.headers.Authorization=`Bearer ${token}`;
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
 });
 
-export const registerUser=(userData)=> API.post('/auth/register',userData);
-export const loginUser=(userData)=>API.post('/auth/login',userData);
+export const registerUser = (userData) => API.post('/auth/register', userData);
+export const loginUser = (userData) => API.post('/auth/login', userData);
 
-export const getJobs=()=>API.get('/jobs');
-export const getJob=(id)=>API.get(`/jobs/${id}`);
-export const createJob=(jobData)=>API.post('/jobs',jobData);
-export const updateJob=(id,jobData)=>API.patch(`/jobs/${id}`,jobData);
-export const deleteJob=(id)=>API.delete(`/jobs/${id}`);
-export const getStats=()=>API.get('/jobs/stats');
+export const getJobs = () => API.get('/jobs');
+export const getJob = (id) => API.get(`/jobs/${id}`);
+export const createJob = (jobData) => API.post('/jobs', jobData);
+export const updateJob = (id, jobData) => API.patch(`/jobs/${id}`, jobData);
+export const deleteJob = (id) => API.delete(`/jobs/${id}`);
+export const getStats = () => API.get('/jobs/stats');
 
 export default API;
